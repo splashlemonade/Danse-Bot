5c6b58765c7fc595444f33bf9e46174b19bd83b6
splashlemonade <37010764+splashlemonade@users.noreply.github.com>
2018-03-03 07:12:43 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,21 +2,15 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('Paladin Danse reporting.');
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
     if (message.content === '!danse') {
     	message.reply('Outstanding.');
-        message.reply('With you standing by me, I feel like I can take on the world.');
-    	message.reply('If I lost you, I don't know what I'd do.');
-        message.reply('Ad Victorium.');
   	}
-
-    
-  	}
-    
 });
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
