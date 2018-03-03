f7a9df6a46482b066c7e2c574a6bac88e0f37698
splashlemonade <37010764+splashlemonade@users.noreply.github.com>
2018-03-03 07:22:05 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,16 @@ client.on('message', message => {
   	}
     
     {
+    if (message.content === '!danse') {
+    	message.reply('With you standing by me, I feel like I can take on the world.');
+  	}
+        
+        {
+    if (message.content === '!danse') {
+    	message.reply('If I lost you, I don't know what I'd do.');
+  	}
+    
+    {
     if (message.content === '!paladin') {
     	message.reply('Ad Victoriam.');
   	}
