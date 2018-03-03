0782a4172ed6dad2c36aca7da013935fe0ba871a
splashlemonade <37010764+splashlemonade@users.noreply.github.com>
2018-03-03 07:29:08 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,19 +10,21 @@ client.on('message', message => {
     	message.reply('Outstanding.');
     }
     
+    {
+    if (message.content === '!dnse') {
+    	message.reply('With you standing by me, I feel like I can take on the world.');
+    }
+        
+        {
     if (message.content === '!johnfield') {
-    message.reply('If I lost you, I don't know what I'd do.');
-}
-          
-    {
-    if (message.content === '!d*nse') {
-    	message.reply('With you standing by me, I feel like I can take on the world..');
-  	}
-    
-    {
+    	message.reply('If I lost you, I dont know what I would do.');
+    }
+            
+            {
     if (message.content === '!paladin') {
     	message.reply('Ad Victoriam.');
-  	}
+    }
+                
 });
 
 
