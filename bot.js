b59b562c8489728256e4a15a26c18d54350d3c40
splashlemonade <37010764+splashlemonade@users.noreply.github.com>
2018-03-03 07:08:09 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,19 +8,14 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === '!danse') {
     	message.reply('Outstanding.');
-  	}
-    
-    if (message.content === '!danse') {
-    	message.reply('Ad Victorium.');
-  	}
-    
-    if (message.content === '!danse') {
-    	message.reply('With you standing by me, I feel like I can take on the world.');
-  	}
-    
-    if (message.content === '!danse') {
+        message.reply('With you standing by me, I feel like I can take on the world.');
     	message.reply('If I lost you, I don't know what I'd do.');
+        message.reply('Ad Victorium.');
   	}
+
+    
+  	}
+    
 });
 
 // THIS  MUST  BE  THIS  WAY
