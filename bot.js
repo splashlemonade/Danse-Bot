a8a1bb691009d90d08782b5281389126c9ca6bb4
splashlemonade <37010764+splashlemonade@users.noreply.github.com>
2018-03-04 05:22:02 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,68 +7,24 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-@client.command(pass_context=True)
-async def insoviet(ctx):
+client.on('message', message => {
+    if (message.content === '!danse')
+string[3] responses = { "Outstanding.", "Ad Victoriam.", "What if nobody shot me and my head just did that?" }; //make an array of strings/responses to choose from
 
-    possible_responses = ["Outstanding.", "With you by my side, I feel like I can take on the world.", "Ad Victoriam.", "What if nobody shot me and my head just did that?"]
+Random rnd = new Random(); //create new Random class
+int i = rnd.Next(0, responses.Length); //Indexing starts from 0, and the max value is minused by 1 (just how Random.Next works) so this will even out perfectly.
 
-    current_response = random.choice(possible_responses)
 
-    await client.say(current_response)
-    }
-  
-}};
+
+e.Channel.SendMessage(responses[i]); //Not sure what e.Channel.SendMessage is in the newer library versions, but that's what it is in 0.9.6 and it should be similar in the newer versions
+
+});
 
 client.on('message', message => {
     if (message.content === '!danse') {
     	message.reply('Outstanding.');
     }
-    
-});
 
-client.on('message', message => {
-    if (message.content === '!johnfield') {
-    	message.reply('With you by my side, I feel like I can take on the world.');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === '!wash') {
-    	message.reply('will you be the rachel to my ross');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === '!jfk') {
-    	message.reply('What if nobody shot me and my head just did that?');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === '!paladin') {
-    	message.reply('Ad Victoriam.');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === '!dash') {
-    	message.reply('I like it when you are this close.');
-  	}
-    
-});
-
-client.on('message', message => {
-    if (message.content === '!Danse') {
-    	message.reply('Godless heathens!');
-  	}
-    
-});
-
-client.on('message', message => {
-    if (message.content === '!DANSE') {
-    	message.reply('I am more of a country-western and bluegrass fan myself.');
-  	}
-    
 });
 
 // THIS  MUST  BE  THIS  WAY
