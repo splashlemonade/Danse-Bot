518299ee2fad86fa713f77f1b24ea92cd8fa658d
splashlemonade <37010764+splashlemonade@users.noreply.github.com>
2018-03-04 05:51:24 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '!danse') {
-        message.reply (var responses = ["Well hello there!","Hello","Hola!"];
+        message.reply var responses = ["Well hello there!","Hello","Hola!"];
 var totalResponses = responses.length;
 var responseIndex = Math.floor((Math.random() * 10) + 1) % totalResponses;
 var response = responses[responseIndex];
