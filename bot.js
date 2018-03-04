a34bc4f4f56f3d9527b51ec1586e8a241b0c1aa0
splashlemonade <37010764+splashlemonade@users.noreply.github.com>
2018-03-04 05:49:46 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -82,13 +82,6 @@ client.on('message', message => {
     
 });
 
-client.on('message', message => {
-    if (message.content === '!danse') {
-    	message.reply('Locked and loaded.');
-    }
-    
-});
-
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
