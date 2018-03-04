657fbd4f011ab4b71763f6f885ef560131cfed0b
splashlemonade <37010764+splashlemonade@users.noreply.github.com>
2018-03-04 05:11:08 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,20 @@
+import random
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
+
+@client.command(pass_context=True)
+async def insoviet(ctx):
+
+    possible_responses = ["Outstanding.", "With you by my side, I feel like I can take on the world.", "Ad Victoriam.", "What if nobody shot me and my head just did that?"]
+
+    current_response = random.choice(possible_responses)
+
+    await client.say(current_response)
 
 client.on('message', message => {
     if (message.content === '!danse') {
